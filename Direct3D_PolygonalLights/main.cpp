0acb34caee4624e4d938447ff8e4d8b43f35680f
Ermith <hyppercrone@gmail.com>
2022-03-28 20:41:11 +0200

    DirectX init and clear screen

--- Direct3D_PolygonalLights/main.cpp
+++ Direct3D_PolygonalLights/main.cpp
@@ -1,9 +1,18 @@
 #include <Windows.h>
 #include <d3d11_1.h>
 #include <directxcolors.h>
+#include <dxgiformat.h>
+#include <wrl.h>
+
+namespace wrl = Microsoft::WRL;
+using wrl::ComPtr;
 
 HINSTANCE g_hInstance;
 HWND g_hWnd;
+ComPtr<ID3D11Device> g_pDevice;
+ComPtr<ID3D11DeviceContext> g_pContext;
+ComPtr<IDXGISwapChain> g_pSwapChain;
+ComPtr<ID3D11RenderTargetView> g_pRenderTarget;
 
 LRESULT CALLBACK window_callback(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	if (uMsg == WM_DESTROY) {
@@ -59,7 +68,49 @@ HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow) {
 }
 
 HRESULT InitDirect3D() {
+	DXGI_SWAP_CHAIN_DESC sd = { 0 };
+	sd.BufferDesc.Width = 0;
+	sd.BufferDesc.Height = 0;
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.BufferDesc.RefreshRate.Numerator = 0;
+	sd.BufferDesc.RefreshRate.Denominator = 0;
+	sd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+	sd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
 
+	sd.SampleDesc.Count = 1;
+	sd.SampleDesc.Quality = 0;
+
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.BufferCount = 1;
+	sd.OutputWindow = g_hWnd;
+	sd.Windowed = TRUE;
+	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	sd.Flags = 0;
+
+	HRESULT result = D3D11CreateDeviceAndSwapChain(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		nullptr,
+		0,
+		nullptr,
+		0,
+		D3D11_SDK_VERSION,
+		&sd,
+		&g_pSwapChain,
+		&g_pDevice,
+		nullptr,
+		&g_pContext
+	);
+
+	ComPtr<ID3D11Resource> pBackBuffer;
+	result = g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Resource), &pBackBuffer);
+	result = g_pDevice->CreateRenderTargetView(
+		pBackBuffer.Get(),
+		nullptr,
+		&g_pRenderTarget
+	);
+
+	return result;
 }
 
 bool PollEvents(MSG* msg) {
@@ -93,9 +144,14 @@ int WINAPI wWinMain(
 	MSG msgBuffer = {0};
 	while (PollEvents(&msgBuffer)) {
 
+
+
 		// DO STUFF
+		g_pContext->ClearRenderTargetView(g_pRenderTarget.Get(), DirectX::Colors::Blue);
 
+
+		// Swap buffers
+		g_pSwapChain->Present(1u, 0u);
 	}
-
 	return (int)msgBuffer.wParam;
 }
